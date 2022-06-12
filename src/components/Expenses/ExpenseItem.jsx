55c449573258e3655fc22b4de69d4937b224246e
Alvin Yanson <yansonalvin29@gmail.com>
2022-06-12 19:54:57 +0800

    react to events

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -5,6 +5,12 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = ({ date, title, amount }) => {
+  const clickHandler = (message) => {
+    console.log(message)
+  }
+  
+
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
@@ -12,6 +18,7 @@ const ExpenseItem = ({ date, title, amount }) => {
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
+      <button onClick={() => clickHandler('test message')}>Change Title</button>
     </Card>
   );
 };
