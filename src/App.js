81f67c3e2ccec32a36c7fdfd5799ed0a8fb7f872
Alvin Yanson <yansonalvin29@gmail.com>
2022-06-12 17:39:29 +0800

    pass props very easy

--- src/App.js
+++ src/App.js
@@ -3,11 +3,21 @@ import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    { title: "Car Insurance", amount: 294.17, date: new Date(2021, 2, 28) },
+    { title: "Car Insurance", amount: 294.17, date: new Date(2021, 2, 28) },
+    { title: "Car Insurance", amount: 294.17, date: new Date(2021, 2, 28) },
+    { title: "Car Insurance", amount: 294.17, date: new Date(2021, 2, 28) },
+  ]
+
   return (
-    <div className="App">
-      <ExpenseItem />
+    <div>
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date.toISOString()} />
+      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date.toISOString()} />
+      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date.toISOString()} />
+      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date.toISOString()} />
     </div>
-  );
+  )
 }
 
 export default App;
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import "./ExpenseItem.css";
 
-const ExpenseItem = () => {
-  const expenseDate = new Date(2021, 2, 29);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 294.67;
-
+const ExpenseItem = ({ date, title, amount }) => {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{date}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
     </div>
   );
