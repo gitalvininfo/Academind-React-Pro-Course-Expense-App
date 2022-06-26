07894429fa0fb1d392d7762babdb6c39c8e2ff23
Alvin Yanson <yansonalvin29@gmail.com>
2022-06-26 18:30:35 +0800

    module 5 - part 8

--- src/components/Chart/ChartBar.jsx
+++ src/components/Chart/ChartBar.jsx
@@ -1,11 +1,24 @@
+import React from "react";
+import "./ChartBar.css";
 
+const ChartBar = ({ value, maxValue, label }) => {
+  let barFillHeight = "0%";
 
-import React from 'react'
+  if (props.max > 0) {
+    barFillHeight = Math.round((value / maxValue) * 100) + "%";
+  }
 
-const ChartBar = () => {
   return (
-    <div>ChartBar</div>
-  )
-}
+    <div className="chart-bar">
+      <div className="chart-bar__inner">
+        <div
+          className="chart-bar__fill"
+          style={{ height: barFillHeight }}
+        ></div>
+      </div>
+      <div className="chart-bar__label">{label}</div>
+    </div>
+  );
+};
 
-export default ChartBar
+export default ChartBar;
