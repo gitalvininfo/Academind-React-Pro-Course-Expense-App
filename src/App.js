7eb6157d0b7ed7e805929f95737c66a7340446cc
Alvin Yanson <yansonalvin29@gmail.com>
2022-06-26 17:21:02 +0800

    module 5 - part 4

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import NewExpense from './components/NewExpense/NewExpense';
 import { useState } from 'react';
 
 const DUMMY_EXPENSES = [
-  { title: "Book 1", amount: 291.17, date: new Date(2022, 2, 28) },
-  { title: "Book 2", amount: 292.17, date: new Date(2022, 3, 28) },
-  { title: "Book 3", amount: 293.17, date: new Date(2022, 4, 28) },
-  { title: "Book 4", amount: 294.17, date: new Date(2022, 5, 28) },
+  { id: 1, title: "Book 1", amount: 291.17, date: new Date(2022, 2, 28) },
+  { id: 2, title: "Book 2", amount: 292.17, date: new Date(2022, 3, 28) },
+  { id: 3, title: "Book 3", amount: 293.17, date: new Date(2022, 4, 28) },
+  { id: 4, title: "Book 4", amount: 294.17, date: new Date(2022, 5, 28) },
 ]
 
 function App() {
